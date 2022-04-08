7cc314721364bb9cc78156d7f2ba801217d60bb6
pportocarrero <655306@soas.ac.uk>
2022-04-08 12:22:32 -0500

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -58,7 +58,7 @@ if sidebar_options == 'Principales indicadores':
 
     kpi_main1, kpi_main2 = st.columns(2)
 
-    kpi_main1.metric('Avance físico de obra', f'{avance_pmrt_actual:,.2%}', f'{delta_avance_pmrt:,.2s}') + ' p.p. a/a')
+    kpi_main1.metric('Avance físico de obra', f'{avance_pmrt_actual:,.2%}', f'{delta_avance_pmrt:,.2s}' + ' p.p. a/a')
 
     kpi_main2.metric("Avance programado", f'{avance_pmrt_prog_actual:,.2%}', str(delta_pmrt_prog) + 'p.p. a/a')
 
