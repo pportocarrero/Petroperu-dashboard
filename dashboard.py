f7328c3ad75ff817e912952bcd9eb028968dff5d
pportocarrero <655306@soas.ac.uk>
2022-04-08 12:05:40 -0500

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -10,17 +10,25 @@ balance_sheet = pd.read_csv('balance_sheet_annual2.csv', delimiter = ';')
 
 income_statement = pd.read_csv('income_statement.csv', delimiter = ';')
 
-# DEFINITIONS
+# AVANCE DEL PMRT
 
 avance_pmrt_actual = 0.9679
 
+avance_pmrt_actual = '{:.2%}'.format(avance_pmrt_actual)
+
 avance_pmrt_anterior = 0.9274
+
+avance_pmrt_anterior = '{:.2%}'.format(avance_pmrt_anterior)
 
 delta_avance_pmrt = avance_pmrt_actual - avance_pmrt_anterior
 
 avance_pmrt_prog_actual = 0.9937
 
+avance_pmrt_prog_actual = '{:.2%}'.format(avance_pmrt_prog_actual)
+
 avance_pmrt_prog_anterior = 0.9310
+
+avance_pmrt_prog_anterior = '{:.2%}'.format(avance_pmrt_prog_anterior)
 
 delta_pmrt_prog = avance_pmrt_prog_actual - avance_pmrt_prog_anterior
 
